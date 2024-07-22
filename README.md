3aa72e91c0bacc03f0673f431bd3dcd29ed9bee7
Michał Kukla <degregar@gmail.com>
2024-07-22 10:47:31 +0200

    working example with docs

---
+++ README.md
@@ -0,0 +1,70 @@
+# SlowTracker to wip.co helper
+
+I want to use SlowTracker to track my tasks, but I also want to use wip.co to publish progress of my work. It helps me with the process, so I don't have to do it manually.
+
+## How it works
+
+If I want to publish my progress to wip.co, I need to add tag #wip to my task in SlowTracker.
+
+Then the script will:
+1. Fetch first task with #wip tag from SlowTracker.
+2. Use OpenAI to translate all the wins to English.
+3. Use AWS SES to send the email to my private email.
+4. Remove #wip tag from the task in SlowTracker and add #wip-added tag.
+
+## Deploy
+
+```bash
+zip -r9 auto-wip.zip .
+aws lambda update-function-code \
+       --function-name auto-wip \
+       --zip-file fileb://auto-wip.zip \
+       --profile auto-wip \
+       --region eu-central-1
+```
+
+## Setup
+
+```bash
+cp .env.example .env
+```
+
+Fill in the `.env` file with your SlowTracker, OpenAI API keys, and email validated in AWS SES.
+
+Create manually `auto-wip` lambda function in AWS console.
+
+```bash
+export $(grep -v '^#' .env | xargs)
+
+aws lambda update-function-configuration --function-name auto-wip \
+  --environment "Variables={SLOWTRACKER_API_KEY=$SLOWTRACKER_API_KEY,OPENAI_API_KEY=$OPENAI_API_KEY,EMAIL=$EMAIL}" \
+  --profile auto-wip \
+  --timeout 30 \
+  --region eu-central-1
+
+aws iam put-role-policy --role-name $ROLE_NAME --policy-name $POLICY_NAME --policy-document file://<(cat <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ],
+      "Resource": "arn:aws:ses:$REGION:$(aws sts get-caller-identity --profile $PROFILE --query Account --output text):identity/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+```
+
+Finally, in AWS console, add a trigger EventBridge (CloudWatch Events) to the lambda function to trigger it, e.g. every day at 8:00 UTC.
---
+++ index.js
@@ -0,0 +1,107 @@
+const axios = require('axios');
+const OpenAI = require('openai');
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses")
+require('dotenv').config();
+
+const sesClient = new SESClient({ region: process.env.SES_REGION });
+
+const getPrompt = (content, tags) => {
+    return `
+You'll get a success, win or a lesson learned from a user. I need you to summarize it shortly in English (one sentence). Write it from the 1st person perspective. At the end of the summary please add the tags: ${tags.filter(tag => tag !== "wip").join(', ')}.
+
+Provided content:
+---
+${content}
+`
+}
+
+const createSendEmailCommand = (toAddress, fromAddress, subject, textBody) => {
+    return new SendEmailCommand({
+        Destination: {
+            ToAddresses: [toAddress],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Charset: "UTF-8",
+                    Data: textBody,
+                },
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: subject,
+            },
+        },
+        Source: fromAddress,
+    });
+};
+
+
+exports.handler = async (event) => {
+    try {
+        // Step 1: Fetch entries from SlowTracker API
+        const response = await axios.get('https://api.slowtracker.com/wins?period=30&tags=wip', {
+            headers: { 'Authorization': `Bearer ${process.env.SLOWTRACKER_API_KEY}` }
+        });
+
+        const entries = response.data;
+        if (entries.length === 0) {
+            console.log('No entries found');
+            return;
+        }
+
+        const entry = entries[0];
+        const { uuid, tags, label: content } = entry;
+
+        if (!content) {
+            console.error('No content found');
+            return;
+        }
+
+        console.info(`Processing entry: ${content}`);
+
+        // Step 2: Translate and summarize content using OpenAI API
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        const prompt = getPrompt(content, tags)
+
+        const gptResponse = await openai.chat.completions.create({
+            model: 'gpt-4o-mini',
+            messages: [{ role: 'user', content: prompt }],
+        });
+
+        const translatedContent = gptResponse.choices[0].message.content.trim();
+
+        console.info('Translated and summarized content:', translatedContent)
+
+
+        // Step 3: Send email using SES
+        const fromAddress = process.env.EMAIL;
+        const toAddress = process.env.EMAIL;
+        const subject = 'Translated and Summarized Entry';
+        const textBody = translatedContent;
+
+        const sendEmailCommand = createSendEmailCommand(toAddress, fromAddress, subject, textBody);
+        try {
+            await sesClient.send(sendEmailCommand);
+            console.log('Email sent successfully');
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
+
+        // Step 4: Update entry with new tags
+        const newTags = tags.filter(tag => tag !== 'wip').concat('wip-added');
+        await axios.patch(`https://api.slowtracker.com/wins/${uuid}`, {
+            tags: newTags
+        }, {
+            headers: { 'Authorization': `Bearer ${process.env.SLOWTRACKER_API_KEY}` }
+        });
+
+        console.log('Process completed successfully');
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
